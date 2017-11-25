758780506ac4ead89eaff0637e8797edb677b83a
LizAharonian <lizaharonian@gmail.com>
2017-11-26 01:08:33 +0200

    fix AIPlayer bug for no moves for opponent

--- src/AIPlayer.cpp
+++ src/AIPlayer.cpp
@@ -5,44 +5,49 @@
 #include "AIPlayer.h"
 pair<int, int> AIPlayer::getInput(vector< pair<int,int> > options, const Board* board,
                                 Board::Cell currentCellType, Board::Cell opponentCellType){
-    for (int i = 0; i<options.size();i++) {
+    /*for (int i = 0; i<options.size();i++) {
         cout << options[i].first+1<<options[i].second+1<<endl;
-    }
+    }*/
     map<pair<int,int>,int> scoresPerOption;
     for(int i=0;i<options.size();i++) {
         Board copyBoard(*board);
         //set the current gameLogicBoard
         (*this).gameLogic->setBoard(&copyBoard);
-        cout<<"copy Boardbefore"<<options[i].first+1<<options[i].second+1<<endl;
-        this->gameLogic->getBoard()->print();
+       // cout<<"copy Boardbefore"<<options[i].first+1<<options[i].second+1<<endl;
+       // this->gameLogic->getBoard()->print();
         (*this).gameLogic->makeMove(options[i].first,options[i].second,currentCellType,opponentCellType);
-        cout<<"copy BoardAfter"<<options[i].first+1<<options[i].second+1<<endl;
-        this->gameLogic->getBoard()->print();
+       // cout<<"copy BoardAfter"<<options[i].first+1<<options[i].second+1<<endl;
+       // this->gameLogic->getBoard()->print();
         //get opponent optional moves, the params are inverse in purpuse
         vector< pair<int,int> > opponentOptionalScors = (*this).gameLogic->possibleMoves(opponentCellType,currentCellType);
+
         int max;
-        int once=0;
-        for(int j=0; j<opponentOptionalScors.size();j++) {
-            Board temp(copyBoard);
-            gameLogic->setBoard(&temp);
-            cout<<"before movment x"<<endl;
-            gameLogic->getBoard()->print();
+        if (opponentOptionalScors.size() != 0) {
+            int once = 0;
+            for (int j = 0; j < opponentOptionalScors.size(); j++) {
+                Board temp(copyBoard);
+                gameLogic->setBoard(&temp);
+              //  cout << "before movment x" << endl;
+                //gameLogic->getBoard()->print();
 
-            gameLogic->makeMove(opponentOptionalScors[j].first , opponentOptionalScors[j].second,
-                                opponentCellType, currentCellType);
-            cout<<"after movment x"<<endl;
-            gameLogic->getBoard()->print();
-            int optionalMax = gameLogic->getScores(opponentCellType, currentCellType);
-            //update the maximum score
-            if (once == 0) {
-                max = optionalMax;
-                once++;
+                gameLogic->makeMove(opponentOptionalScors[j].first, opponentOptionalScors[j].second,
+                                    opponentCellType, currentCellType);
+                //cout << "after movment x" << endl;
+               // gameLogic->getBoard()->print();
+                int optionalMax = gameLogic->getScores(opponentCellType, currentCellType);
+                //update the maximum score
+                if (once == 0) {
+                    max = optionalMax;
+                    once++;
+                }
+
+                if (optionalMax >= max) {
+                    max = optionalMax;
+                }
+
             }
-
-            if (optionalMax >= max) {
-                max = optionalMax;
-            }
-
+        }else {
+            max = gameLogic->getScores(opponentCellType, currentCellType);
         }
         scoresPerOption[options[i]] = max;
 
@@ -57,12 +62,12 @@ pair<int, int> AIPlayer::getInput(vector< pair<int,int> > options, const Board* 
         }
     }
 
-    (*board).print();
+    //(*board).print();
     //return the selected move
     return chosenMove;
 
 }
 
 AIPlayer::~AIPlayer() {
-    delete (*this).gameLogic;
+   // delete (*this).gameLogic;
 }
